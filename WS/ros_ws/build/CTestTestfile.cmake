cde160e041350b3190d7d0b5b7939da7d3c68767
BazingoW <ramiro.animus@gmail.com>
2019-05-10 21:26:30 +0100

    ws is now in repo

---
+++ WS/ros_ws/build/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/mario/ros_ws/src
+# Build directory: /home/mario/ros_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
+subdirs(convert_bag_images)
+subdirs(multiple_kinects_launch)
---
+++ WS/ros_ws/build/convert_bag_images/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/mario/ros_ws/src/convert_bag_images
+# Build directory: /home/mario/ros_ws/build/convert_bag_images
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
