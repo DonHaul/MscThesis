04bcb7472157e5787556ca8764b2c69969fa1027
BazingoW <ramiro.animus@gmail.com>
2019-08-05 12:59:13 +0100

    boom

--- Code/replacer.py
+++ Code/replacer.py
@@ -1,16 +1,19 @@
 import os
 from distutils.dir_util import copy_tree
 import libs
+import shutil
 
-ola = os.listdir('./Logs')
+ola = os.listdir('./errors')
 
 
 for o in ola:
     
-    newO =  o.replace("|", "_")
+    newO =  o.replace(":", "_")
     print(newO)
     if newO == o:
         continue
     
-    libs.FileIO.CreateFolder('./Logs/' + newO,putDate=False)
-    copy_tree('./Logs/' + o ,'./Logs/' + newO)
+    libs.FileIO.CreateFolder('./errors/' + newO,putDate=False)
+    copy_tree('./errors/' + o ,'./errors/' + newO)
+
+    shutil.rmtree('./errors/' + o)
